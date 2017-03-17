73a71f57275eaed075fd899f3d102031f9b47ab3
jc350949 <cameron.gauci@my.jcu.edu.au>
2017-03-17 13:57:31 +1000

    Add Function

--- oddName.py
+++ oddName.py
@@ -1,11 +1,22 @@
-"""
-Cameron
-"""
-name = input("What is your Name?")
-name = "".join(name.split())
-count = 0
 
-if len(name) > 1:
-    print(name[::2])
-else:
-    print("Not a valid name")
+def main():
+  get_name()
+
+
+def get_name():
+    name = input("What is your Name?")
+    name = "".join(name.split())
+    count = 0
+    if len(name) > 1:
+        print(name[::2])
+    else:
+        print("Not a valid name")
+
+
+main()
+
+
+
+
+
+
